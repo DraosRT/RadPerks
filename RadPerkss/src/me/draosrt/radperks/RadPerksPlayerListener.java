b9e8e614505785c5d7944eafe044cfc51245e4a2
DraosRT <draos@live.com>
2011-12-04 20:05:24 +0800

    Update RadPerkss/src/me/draosrt/radperks/RadPerksPlayerListener.java

--- RadPerkss/src/me/draosrt/radperks/RadPerksPlayerListener.java
+++ RadPerkss/src/me/draosrt/radperks/RadPerksPlayerListener.java
@@ -1,5 +1,43 @@
-package me.draosrt.radperks;
+package me.rohan.RadPerks;
 
-public class RadPerksPlayerListener {
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class RadPerksPlayerListener extends PlayerListener {
+
+  
+	public RadPerksPlayerListener(RadPerks instance) {
+		
+	}
+	
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		if (event.getItem().getType()== Material.RAW_BEEF) {
+            if (event.getAction()==Action.RIGHT_CLICK_AIR) {
+            	player.sendMessage(ChatColor.GREEN + "It Works!");
+
+                
+		
+		}
+	}
+		if (event.getItem().getType()== Material.RAW_FISH) {
+            if (event.getAction()==Action.RIGHT_CLICK_AIR) {
+            	player.sendMessage(ChatColor.GREEN + "It Works!");
+}
+
 
 }
+		if (event.getItem().getType()== Material.RAW_CHICKEN) {
+            if (event.getAction()==Action.RIGHT_CLICK_AIR) {
+            	player.sendMessage(ChatColor.GREEN + "It Works!");
+	
+	}
+}
+	}
+}
+
+
